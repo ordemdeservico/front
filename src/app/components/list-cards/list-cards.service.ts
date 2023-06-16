91ba5d9571b446543cbf2984f8df7acaa5566d89
Matheus <matheus.gmuccio@gmail.com>
2023-06-16 08:56:56 -0300

    feat: create request aprove and decline os

--- src/app/components/list-cards/list-cards.service.ts
+++ src/app/components/list-cards/list-cards.service.ts
@@ -9,36 +9,39 @@ import { environment } from 'src/environments/environment';
 const API = environment.API;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ListCardsService {
   private selectedOrderService!: OrderService;
- 
-  constructor(private http: HttpClient, private tokenService : TokenService) { }
+
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   getOsByFilter(filtros: any[]): Observable<any> {
     let params = new HttpParams();
-  
+
     if (filtros) {
       for (const filter of filtros) {
-        if (filter.filter === 'setor_principal_id' || filter.filter === 'status_os' || filter.filter === 'nivel_prioridade') {
+        if (
+          filter.filter === 'setor_principal_id' ||
+          filter.filter === 'status_os' ||
+          filter.filter === 'nivel_prioridade'
+        ) {
           params = params.append(filter.filter, filter.value);
         }
       }
-  
     }
-    
+
     const options = { params: params };
-    return this.http.get(`${API}/ordem-servico/admin/filtros`, options );
+    return this.http.get(`${API}/ordem-servico/admin/filtros`, options);
   }
 
-  getCardInfos(): Observable<any>{
+  getCardInfos(): Observable<any> {
     return this.http.get(`${API}/ordem-servico/`);
   }
 
   getAllTec(): Observable<any> {
     return this.http.get(`${API}/user/tecnico`);
-  }  
+  }
 
   getAllServices(): Observable<any> {
     return this.http.get(`${API}/tipo-servico/`);
@@ -52,17 +55,29 @@ export class ListCardsService {
     let params = new HttpParams();
 
     if (setor) {
-      params = params.append('SetorPrincipal', setor)
+      params = params.append('idSetorPrincipal', setor);
     }
     const options = { params: params };
-    console.log(params)
-    return this.http.get(`${API}/setor-secundario/instituicao`, options );
+    return this.http.get(`${API}/setor-secundario/instituicao`, options);
   }
 
   getAllSetorSecundarios(): Observable<any> {
     return this.http.get(`${API}/setor-secundario/`);
-  
   }
+
+aprovarOs(params: any): Observable<any> {
+  let httpParams = new HttpParams();
+  
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      httpParams = httpParams.append(key, params[key]);
+    }
+  }
+  
+  console.log(httpParams);
+  return this.http.patch<any>(`${API}/ordem-servico/aprovar`, httpParams);
+}
+
 
   setSelectedOrderService(orderService: OrderService): void {
     this.selectedOrderService = orderService;
@@ -71,5 +86,4 @@ export class ListCardsService {
   getSelectedOrderService(): OrderService {
     return this.selectedOrderService;
   }
-
 }
