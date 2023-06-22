5dbc964f320045cce6807b94bb5514801ae861d5
Matheus <matheus.gmuccio@gmail.com>
2023-06-22 14:36:26 -0300

    feat: configurating cards for user

--- src/app/pages/dashboard-user/dashboard-user.component.ts
+++ src/app/pages/dashboard-user/dashboard-user.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-user',
   templateUrl: './dashboard-user.component.html',
   styleUrls: ['./dashboard-user.component.scss']
 })
-export class DashboardUserComponent {
+export class DashboardUserComponent implements OnInit {
 
+  constructor(
+    private router: Router
+  ) { }
+  
+  navigateToForm(){
+    this.router.navigate(['/home-form'])
+  }
+
+  
+  ngOnInit(): void {
+      
+  }
+ 
 }
--- src/app/pages/dashboard-user/dashboard-user.module.ts
+++ src/app/pages/dashboard-user/dashboard-user.module.ts
@@ -5,7 +5,9 @@ import { DashboardUserRoutingModule } from './dashboard-user-routing.module';
 import { DashboardUserComponent } from './dashboard-user.component';
 import { MenuModule } from 'src/app/shared/menu/menu.module';
 import { CardModule } from 'src/app/components/card/card.module';
-
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { ListCardsModule } from 'src/app/components/list-cards/list-cards.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import { CardModule } from 'src/app/components/card/card.module';
     CommonModule,
     DashboardUserRoutingModule,
     MenuModule,
-    CardModule
+    CardModule,
+    ButtonModule,
+    FormsModule,
+    ListCardsModule
   ]
 })
 export class DashboardUserModule { }
