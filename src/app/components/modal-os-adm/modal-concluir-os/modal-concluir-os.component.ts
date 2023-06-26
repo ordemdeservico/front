2876a005540f5bd42ffbe9fab8b584199e238f57
Matheus <matheus.gmuccio@gmail.com>
2023-06-26 14:07:07 -0300

    feat: add method concluir os

--- src/app/components/modal-os-adm/modal-concluir-os/modal-concluir-os.component.ts
+++ src/app/components/modal-os-adm/modal-concluir-os/modal-concluir-os.component.ts
@@ -1,15 +1,67 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { OrderService } from 'src/app/shared/models/order-service.model';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ListCardsService } from '../../list-cards/list-cards.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-modal-concluir-os',
   templateUrl: './modal-concluir-os.component.html',
-  styleUrls: ['./modal-concluir-os.component.scss']
+  styleUrls: ['./modal-concluir-os.component.scss'],
+  providers: [ DatePipe ]
 })
-export class ModalConcluirOsComponent {
+export class ModalConcluirOsComponent implements OnInit {
 
   @Input("orderService") orderService?: OrderService;
+  @Input() role?: string;
+  @Output() attCards = new EventEmitter();
 
+  formGroup: FormGroup;
+
+  constructor(
+    private listCardsService: ListCardsService,
+    private formBuilder: FormBuilder,
+    private datePipe: DatePipe,
+  ) {
+    this.formGroup = this.formBuilder.group({
+      material: ['', Validators.required],
+      relatorio: ['', Validators.required],
+    });
+   }
+
+  obterDataAtual(): string {
+    const data_insercao = new Date();
+    return this.datePipe.transform(data_insercao, 'yyyy/MM/dd') || ''; 
+  }
+
+  concluirOs() {
+    if (this.formGroup.valid) {
+
+      const values: any =  {
+        ordem_servico_id: this.orderService!.id,
+        data_final: this.obterDataAtual(),
+        material: this.formGroup.value.material,
+        relatorio: this.formGroup.value.relatorio
+      }
+
+      this.listCardsService.concluirOs(values).subscribe({
+        next: (res) => {
+          console.log(res);
+          console.log(values)
+          this.attCards.emit(true);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+      console.log('values: ', this.formGroup.value)
+    }
+  }
+
+
+  ngOnInit(): void {
+      // console.log('Concluir: ', this.role)
+  }
 
 
 }
