d8e4006030e6a3e46a3cf75dcf3c627ef99c33fa
Vinícius La Serra <laserra89@gmail.com>
2023-06-27 10:19:37 -0300

    Merge branch 'master' of https://github.com/ordemdeservico/front

--- src/app/pages/home-form/home-form.module.ts
+++ src/app/pages/home-form/home-form.module.ts
@@ -4,17 +4,24 @@ import { CommonModule } from '@angular/common';
 import { HomeFormRoutingModule } from './home-form-routing.module';
 import { HomeFormComponent } from './home-form.component';
 import { MenuModule } from 'src/app/shared/menu/menu.module';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 
 @NgModule({
   declarations: [
-    HomeFormComponent
+    HomeFormComponent,
   ],
   imports: [
     CommonModule,
     HomeFormRoutingModule,
-    MenuModule
-
+    MenuModule,
+    DropdownModule,
+    InputTextareaModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeFormModule { }
