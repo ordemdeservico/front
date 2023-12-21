0a44e5a384527f1e484229e6da2e184e74442c2a
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2023-12-21 11:41:17 -0300

    fix: remove old codes and fix requests

--- src/app/components/modal-os-adm/modal-aprovar-os/modal-aprovar-os.component.ts
+++ src/app/components/modal-os-adm/modal-aprovar-os/modal-aprovar-os.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, OnInit, Output, EventEmitter, SimpleChanges, OnChanges, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { ListCardsService } from '../../list-cards/list-cards.service';
 import { Validators, FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { OrderService } from 'src/app/shared/models/order-service.model';
 import { Observable, lastValueFrom, map } from 'rxjs';
-import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { MessageService, ConfirmationService } from 'primeng/api';
-import { Galleria } from 'primeng/galleria';
 
 interface DropdownOptions {
   label: string;
@@ -22,29 +20,7 @@ export class ModalAprovarOsComponent implements OnInit {
   @Input('orderService') orderService?: OrderService;
   @Input() displayModal?: boolean;
   @Output() attCards = new EventEmitter();
-
-  @ViewChild('galleria') galleria: Galleria | undefined;
   images: any[] = [];
-  position: string = 'bottom';
-
-  fullscreen: boolean = false;
-  activeIndex: number = 0;
-  onFullScreenListener: any;
-
-  responsiveOptions: any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5,
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3,
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1,
-    },
-  ];
 
 
 
@@ -60,11 +36,9 @@ export class ModalAprovarOsComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private ref: DynamicDialogRef,
     private listCardsService: ListCardsService,
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
-    private cd: ChangeDetectorRef
   ) {
     this.formGroup = this.formBuilder.group({
       setor_principal_id: ['', Validators.required],
@@ -95,14 +69,15 @@ export class ModalAprovarOsComponent implements OnInit {
           detail: 'Usuário excluido.',
           life: 3000,
         });
-        this.listCardsService.declinarOs(this.orderService!.id).subscribe(
-          (res) => {
+
+        this.listCardsService.declinarOs(this.orderService!.id).subscribe({
+          next: (res) => {
             this.attCards.emit(true);
           },
-          (err) => {
-            console.error(err);
+          error: (erro) => {
+            console.error('Erro: ', erro);
           }
-        );
+        })
       },
       acceptButtonStyleClass: 'custom-accept-button',
       rejectButtonStyleClass: 'custom-reject-button',
@@ -122,9 +97,8 @@ export class ModalAprovarOsComponent implements OnInit {
             parametros[propriedade] = formValues[propriedade];
           }
         }
-
-        this.listCardsService.aprovarOs(parametros).subscribe(
-          (res) => {
+        this.listCardsService.aprovarOs(parametros).subscribe({
+          next: (res) => {
             console.log(res);
             this.messageService.add({
               severity: 'success',
@@ -133,10 +107,10 @@ export class ModalAprovarOsComponent implements OnInit {
             });
             this.attCards.emit(true);
           },
-          (err) => {
-            console.error(err);
+          error: (erro) => {
+            console.error('Erro: ', erro);
           }
-        );
+        });
       }
     } else {
       this.messageService.add({
@@ -161,84 +135,9 @@ export class ModalAprovarOsComponent implements OnInit {
     return true;
   }
 
-
-  toggleFullScreen() {
-    if (this.fullscreen) {
-        this.closePreviewFullScreen();
-    } else {
-        this.openPreviewFullScreen();
-    }
-
-    this.cd.detach();
-  }
-
-  openPreviewFullScreen() {
-    let elem = this.galleria?.element.nativeElement.querySelector('.p-galleria');
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem['mozRequestFullScreen']) {
-        /* Firefox */
-        elem['mozRequestFullScreen']();
-    } else if (elem['webkitRequestFullscreen']) {
-        /* Chrome, Safari & Opera */
-        elem['webkitRequestFullscreen']();
-    } else if (elem['msRequestFullscreen']) {
-        /* IE/Edge */
-        elem['msRequestFullscreen']();
-    }
-  }
-
-  onFullScreenChange() {
-    this.fullscreen = !this.fullscreen;
-    this.cd.detectChanges();
-    this.cd.reattach();
-  }
-
-  closePreviewFullScreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if ((document as any)['mozCancelFullScreen']) {
-      (document as any)['mozCancelFullScreen']();
-    } else if ((document as any)['webkitExitFullscreen']) {
-      (document as any)['webkitExitFullscreen']();
-    } else if ((document as any)['msExitFullscreen']) {
-      (document as any)['msExitFullscreen']();
-    }
-  }
-  bindDocumentListeners() {
-    this.onFullScreenListener = this.onFullScreenChange.bind(this);
-    document.addEventListener('fullscreenchange', this.onFullScreenListener);
-    document.addEventListener('mozfullscreenchange', this.onFullScreenListener);
-    document.addEventListener('webkitfullscreenchange', this.onFullScreenListener);
-    document.addEventListener('msfullscreenchange', this.onFullScreenListener);
-  }
-
-  unbindDocumentListeners() {
-    document.removeEventListener('fullscreenchange', this.onFullScreenListener);
-    document.removeEventListener('mozfullscreenchange', this.onFullScreenListener);
-    document.removeEventListener('webkitfullscreenchange', this.onFullScreenListener);
-    document.removeEventListener('msfullscreenchange', this.onFullScreenListener);
-    this.onFullScreenListener = null;
-  }
-
-  galleriaClass() {
-    return `custom-galleria ${this.fullscreen ? 'fullscreen' : ''}`;
-  }
-
-  fullScreenIcon() {
-    return `pi ${this.fullscreen ? 'pi-window-minimize' : 'pi-window-maximize'}`;
-  }
-
-
-  ngOnDestroy() {
-    this.unbindDocumentListeners();
-  }
-
-
   async ngOnInit() {
     if (this.orderService?.images?.type1) {
       for (let image of this.orderService.images.type1) {
-        // this.images.push(image.img_key.toString());
         const imgKey = image.img_key.toString();
         this.images.push(imgKey);
 
@@ -250,7 +149,6 @@ export class ModalAprovarOsComponent implements OnInit {
         };
       }
     }
-    this.bindDocumentListeners();
 
 
 
